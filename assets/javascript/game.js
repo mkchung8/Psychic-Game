cd23c017c16cc4adbf05510aaebf21850324bef2
Miyoung Chung <mkchung.8@gmail.com>
2019-09-05 11:43:43 -0700

    Added structural HTML elements and CSS styling with javascript pseudocoding.

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+//List array of computer choices 
+var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+
+//Creating variables to hold the starting number of wins, losses, and gueses left
+
+//Creating variables to hold reference places in the HTML elements we need to manipulate. 
+
+//Computer generates random letter from the alphabet
+
+//Run function when uses presses the key
+
+//User generates guess by pressing a key 
+
+//if computer guess === user guess then increase wins by one
+//else user guesses again, previous guess added to screen, guesses left decreases by one
+//until 10 guesses, losses increases by one 
+
+//continue function until 10 wins or 10 losses is reached (reset function)
